d0bb7958d42cdbd7572d4ce647149c33d075133d
YagoSantoss <yagomib971@gmail.com>
2025-08-04 20:22:40 -0300

    codico pronto

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+
+function adicionarNome() {
+  let input = document.getElementById("amigo");
+  let nome = input.value.trim();
+
+ 
+  if (nome === "") {
+    alert("Insira um nome, por favor.");
+    return;
+  }
+
+ 
+  amigos.push(nome);
+
+  
+  atualizarLista();
+
+  
+  input.value = "";
+}
+
+
+function atualizarLista() {
+  let lista = document.getElementById("listaAmigos");
+
+  
+  lista.innerHTML = "";
+
+  
+  if (amigos.length > 0) {
+    
+    for (let i = 0; i < amigos.length; i++) {
+      const nome = amigos[i];
+      lista.innerHTML += `<li>${nome}</li>`;
+    }
+  } else {
+    
+    lista.innerHTML = "<li>Nenhum amigo adicionado ainda.</li>";
+  }
+}
+
+
+function sortearAmigo() {
+  
+  if (amigos.length === 0) { 
+    alert("A lista de amigos está vazia. Adicione nomes antes de sortear.");
+    return;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+  
+  const nomeSorteado = amigos[indiceAleatorio];
+
+  
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>Amigo sorteado: <strong>${nomeSorteado}</strong></li>`;
+}
